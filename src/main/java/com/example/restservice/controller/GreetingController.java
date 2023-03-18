d667db317aa6d7382438661898b68eca41c424c5
Stepan <crayon-d@mail.ru>
2023-03-18 17:33:31 +0300

    initial code. start web server. RESTful

--- src/main/java/com/example/restservice/controller/GreetingController.java
+++ src/main/java/com/example/restservice/controller/GreetingController.java
@@ -1,10 +1,9 @@
-package com.example.restservice;
+package com.example.restservice.controller;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.restservice.dto.Greeting;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
@@ -15,5 +14,15 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@PostMapping("/greeting")
+	public Greeting greetingPost(@RequestBody Greeting greeting) {
+		return new Greeting(counter.incrementAndGet(), greeting.content());
+	}
+
+	@GetMapping("/healthcheck")
+	public String healthcheck() {
+		return "Ok";
 	}
 }
--- src/main/java/com/example/restservice/dto/Greeting.java
+++ src/main/java/com/example/restservice/dto/Greeting.java
@@ -1,3 +1,3 @@
-package com.example.restservice;
+package com.example.restservice.dto;
 
 public record Greeting(long id, String content) { }
